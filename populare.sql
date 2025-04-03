ee983f414405a5c1d66fbb47401aca1deccaa176
paulcojocaru3 <148447157+paulcojocaru3@users.noreply.github.com>
2025-04-04 01:23:28 +0300

    Script populare mySQL

---
+++ populare.sql
@@ -0,0 +1,11 @@
+INSERT IGNORE INTO users (username, password, access) VALUES
+('user1', 'abc123', 'USER'),
+('user2', 'abc123', 'USER'),
+('admin1', 'adminpass', 'ADMIN'),
+('user3', 'abc123', 'USER'),
+('admin2', 'adminpass', 'ADMIN')
+('user4', 'abc123', 'USER'),
+('user5', 'abc123', 'USER'),
+('admin3', 'adminpass', 'ADMIN'),
+('user6', 'abc123', 'USER'),
+('admin4', 'adminpass', 'ADMIN');
