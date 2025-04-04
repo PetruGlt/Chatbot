7798609d5bf612e852faacfa283c63b16c04e4c3
Rimniceanu-Alex <alexrimniceanu@yahoo.com>
2025-04-04 12:48:45 +0300

    Tweaked sql script + changed Port

--- populare.sql
+++ populare.sql
@@ -1,11 +1,11 @@
 INSERT IGNORE INTO users (username, password, access) VALUES
-('user1', 'abc123', 'USER'),
-('user2', 'abc123', 'USER'),
-('admin1', 'adminpass', 'ADMIN'),
-('user3', 'abc123', 'USER'),
-('admin2', 'adminpass', 'ADMIN')
-('user4', 'abc123', 'USER'),
-('user5', 'abc123', 'USER'),
-('admin3', 'adminpass', 'ADMIN'),
-('user6', 'abc123', 'USER'),
-('admin4', 'adminpass', 'ADMIN');
+('user1', 'abc111', 'USER'),
+('user2', 'abc222', 'USER'),
+('admin1', 'adminpass1', 'ADMIN'),
+('user3', 'abc333', 'USER'),
+('admin2', 'adminpass2', 'ADMIN'),
+('user4', 'abc444', 'USER'),
+('user5', 'abc555', 'USER'),
+('admin3', 'adminpass3', 'ADMIN'),
+('user6', 'abc666', 'USER'),
+('admin4', 'adminpass4', 'ADMIN');
