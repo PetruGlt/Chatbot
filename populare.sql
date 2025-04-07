ac73b90c35a43f4841412b75be77ed461f526310
paulcojocaru3 <148447157+paulcojocaru3@users.noreply.github.com>
2025-04-07 08:48:16 +0300

    Update populare.sql - EXPERT

--- populare.sql
+++ populare.sql
@@ -1,11 +1,11 @@
 INSERT IGNORE INTO users (username, password, access) VALUES
 ('user1', 'abc111', 'USER'),
 ('user2', 'abc222', 'USER'),
-('admin1', 'adminpass1', 'ADMIN'),
+('expert1', 'expertpass1', 'EXPERT'),
 ('user3', 'abc333', 'USER'),
-('admin2', 'adminpass2', 'ADMIN'),
+('expert2', 'expertpass2', 'EXPERT'),
 ('user4', 'abc444', 'USER'),
 ('user5', 'abc555', 'USER'),
-('admin3', 'adminpass3', 'ADMIN'),
+('expert3', 'expertpass3', 'EXPERT'),
 ('user6', 'abc666', 'USER'),
-('admin4', 'adminpass4', 'ADMIN');
+('expert4', 'expertpass4', 'EXPERT');
