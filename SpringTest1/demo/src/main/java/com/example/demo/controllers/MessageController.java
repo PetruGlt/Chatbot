4256cb9d04326ecf8a6defad0d04673164bbea51
Rimniceanu-Alex <alexrimniceanu@yahoo.com>
2025-04-26 05:11:30 +0300

    Fixed Some path url's and Front-Back message handling

--- SpringTest1/demo/src/main/java/com/example/demo/controllers/MessageController.java
+++ SpringTest1/demo/src/main/java/com/example/demo/controllers/MessageController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.QuestionObject;
 import com.example.demo.services.MessageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class MessageController {
@@ -16,16 +19,20 @@ public class MessageController {
     }
 
     @PostMapping("/ask")
-    @ResponseBody
-    public String sendMessage(
-            @RequestParam("question") String question,
-            @RequestParam("username") String username,
-            @RequestParam("conversationId") String conversationId) {
-
-
-        return messageService.sendQuestion(question, username);
+    @ResponseBody//Frontul trimite si asteapta JSON
+    public Map<String, String> sendMessage(@RequestBody QuestionObject dto) {
+        String answer = messageService.sendQuestion(dto.question, dto.username);
+        // Returnam un obiect Json: { "answer": "Hello!" }
+        return Map.of("answer", answer);
     }
-
+//    public String sendMessage(
+//            @RequestParam("question") String question,
+//            @RequestParam("username") String username,
+//            @RequestParam("conversationId") String conversationId) {
+//
+//
+//        return messageService.sendQuestion(question, username);
+//    }
 
 
 
