528ee2f8b0e489234a73899c8b56dfc90f7d21cd
AngelBeji05 <72660450+AngelBeji05@users.noreply.github.com>
2025-05-06 22:47:05 +0300

    Feature/create conversation service (#31)
    * train and create model context
    
    * create conversation service and add history as parameter to chatbot service

--- aimodule/src/chatbot/chatbot.module.ts
+++ aimodule/src/chatbot/chatbot.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatbotController } from './chatbot.controller';
 import { ChatbotService } from './chatbot.service';
+import { ConversationService } from 'src/conversation/conversation.service';
 
 @Module({
   controllers: [ChatbotController],
-  providers: [ChatbotService],
+  providers: [ChatbotService, ConversationService],
 })
 export class ChatbotModule {}
